40a133079ff959c7d38e6e4ea21bcd93efe9c2b8
LordAnthrax94 <andreagi1994@gmail.com>
2025-05-28 16:41:45 +0200

    Feat: Eseguito Milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
-import{ useState } from 'react';
+import{ useState, useEffect } from 'react';
 
 function App() {
   
-  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('');
+  const [products, setProducts] = useState([]);
+  console.log(products);
+  
+
+   useEffect(() => {
+    if(!query.trim()){
+      setProducts([]);
+      return;
+    }
+    fetch(`http://localhost:3333/products?search=${query}`)
+    .then(res => res.json())
+    .then(data => setProducts(data))
+    .catch(error => console.error(error));
+  }, [query]);
 
   return (
-    <>
-    <input 
-    type="text" 
-    placeholder="Cosa vuoi cercare?"
-    value=""
-    key=""
-    />
-     
-    </>
+    <div>
+      <h1>Ricerca</h1>
+      <input 
+      type="text" 
+      placeholder="Cosa vuoi cercare?"
+      value={query}
+      onChange={e => setQuery(e.target.value)}
+      />
+     {products.length > 0 && (<div className="results">
+      {products.map(product => (
+        <p key={product.id}>{product.name}
+
+        </p>
+      ))}
+     </div>)} 
+    </div>
   )
 }
 
